381eabf16dce7f626417e3c89b3d45ecefb726db
PAVAN KUMAR REDDY <pavankumart46@gmail.com>
2020-01-31 18:35:21 +0530

    day two commit

---
+++ src/FunctionsFile.kt
@@ -0,0 +1,14 @@
+fun main(){
+    /*this is the place where our program execution begins*/
+    println(addTwoNumbers(20,30,40))
+}
+
+fun addTwoNumbers(a:Int,b:Int):Int{
+    /*This function adds up two values*/
+    println("Kotlin is wonderful")
+    return a+b
+}
+
+fun addTwoNumbers(a:Int,b:Int,c:Int):Int{
+    return a+b+c
+}
--- src/RockPaperScissor.kt
+++ src/RockPaperScissor.kt
@@ -1,10 +1,19 @@
 fun main(){
     /*let us allow the computer to choose one option from the three options randomly*/
     val options = arrayOf("Rock","Paper","Scissor")
-    val com_index = (Math.random()*(options.size)).toInt()
-    val computer_choice = options[com_index]
-
+    val computer_choice = computerChoice(options)
     /*Lets Get the User Selection*/
+    val user_choice = userChoice(options)
+    /*Decide who won!*/
+    whoIsTheWinner(computer_choice,user_choice,options)
+}
+
+fun computerChoice(options:Array<String>):String{
+    val com_index = (Math.random()*(options.size)).toInt()
+    return options[com_index]
+}
+
+fun userChoice(options:Array<String>):String{
     println("Please Choose an option from the following\n1.Rock\n2.Paper\n3.Scissor")
     val choice = Integer.valueOf(readLine())
     var user_choice = ""
@@ -14,15 +23,15 @@ fun main(){
         3->user_choice = options[choice-1]
         else-> println("The option that you entered is not present!")
     }
+    return user_choice
+}
 
-    /*Decide who won!*/
+fun whoIsTheWinner(computer_choice:String,user_choice:String,options:Array<String>){
     if(computer_choice.equals(user_choice))
         println("Computer selected ${computer_choice} and you selected ${user_choice} - Draw!")
-    else if(computer_choice.equals(options[0]) && user_choice.equals(options[1]))
-        println("Computer selected ${computer_choice} and you selected ${user_choice} - You win!")
-    else if(computer_choice.equals(options[1]) && user_choice.equals(options[2]))
-        println("Computer selected ${computer_choice} and you selected ${user_choice} - You win!")
-    else if(computer_choice.equals(options[2]) && user_choice.equals(options[0]))
+    else if((computer_choice.equals(options[0]) && user_choice.equals(options[1]))
+        || (computer_choice.equals(options[1]) && user_choice.equals(options[2]))
+        || (computer_choice.equals(options[2]) && user_choice.equals(options[0])))
         println("Computer selected ${computer_choice} and you selected ${user_choice} - You win!")
     else
         println("Computer selected ${computer_choice} and you selected ${user_choice} - You loose!")
